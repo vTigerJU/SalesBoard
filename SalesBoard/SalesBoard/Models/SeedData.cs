50027f8f72330aa185d9bc295bed3298f3a41e68
vTigerJU <tivi22ty@student.ju.se>
2024-09-13 13:02:32 +0930

    finished

--- SalesBoard/SalesBoard/Models/SeedData.cs
+++ SalesBoard/SalesBoard/Models/SeedData.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using SalesBoard.Data;
+using System.Runtime.Intrinsics.X86;
 
 namespace SalesBoard.Models
 {
@@ -14,9 +16,12 @@ namespace SalesBoard.Models
             var context = new ApplicationDbContext(serviceProvider.GetRequiredService<DbContextOptions<ApplicationDbContext>>());
 
             SeedSalesStatistics(context);
+            await SeedUsers(userManager, context);
+            SeedItems(context);
             EnsureRoles(context);
             await EnsureAdmin(userManager);
             context.SaveChanges();
+
         }
 
         private static void SeedSalesStatistics(ApplicationDbContext context) 
@@ -28,7 +33,82 @@ namespace SalesBoard.Models
                 statistics.Commission = 0;
                 statistics.CommissionRate = 0.01;
                 context.SalesStatistics.Add(statistics);
+                context.SaveChanges();
+
             }
+        }
+        private static void SeedItems(ApplicationDbContext context) 
+        { 
+            var users = context.Users.ToArray();
+            if (!users.IsNullOrEmpty() && !context.Item.Any())
+            {
+                context.Item.AddRange(
+                   new Item
+                   {
+                       Name = "Ball",
+                       Description = "Round and bouncy",
+                       Price = 10,
+                       Quantity = 5,
+                       User = users[0]
+                   },
+                   new Item
+                   {
+                       Name = "Not Ball",
+                       Description = "Square and not bouncy",
+                       Price = 100,
+                       Quantity = 2,
+                       User = users[0]
+
+                   },
+                   new Item
+                   {
+                       Name = "Bike",
+                       Description = "Needs minor repairs",
+                       Price = 30,
+                       Quantity = 1,
+                       User = users[1]
+
+                   }
+               );
+                context.SaveChanges();
+
+
+            }
+        }
+
+        private static async Task SeedUsers(UserManager<ApplicationUser> userManager, ApplicationDbContext context)
+        {
+            if (!context.Users.Any())
+            {
+                var userEmail = "user1@gmail.com";
+                var userPassword = "Hi123!";
+                var user1 = new ApplicationUser
+                {
+                    Name = "John Doe",
+                    NormalizedUserName = userEmail.ToUpper(),
+                    UserName = userEmail,
+                    Email = userEmail,
+                    NormalizedEmail = userEmail.ToUpper()
+                };
+                userEmail = "user2@gmail.com";
+                var user2 = new ApplicationUser
+                {
+                    Name = "Smith Smithson",
+                    NormalizedUserName = userEmail.ToUpper(),
+                    UserName = userEmail,
+                    Email = userEmail,
+                    NormalizedEmail = userEmail.ToUpper()
+                };
+
+                var result = await userManager.CreateAsync(user1, userPassword);
+                if (result.Succeeded) { await userManager.AddToRoleAsync(user1, "User"); }
+                result = await userManager.CreateAsync(user2, userPassword);
+                if (result.Succeeded) { await userManager.AddToRoleAsync(user2, "User"); }
+                context.SaveChanges();
+
+              
+            }
+
         }
 
         private static void EnsureRoles(ApplicationDbContext context)
@@ -48,6 +128,8 @@ namespace SalesBoard.Models
                         NormalizedName = "USER"
                     }
                 );
+                context.SaveChanges();
+
             }
 
         }
@@ -72,6 +154,7 @@ namespace SalesBoard.Models
                 if (result.Succeeded)
                 {
                     await userManager.AddToRoleAsync(adminUser, "Admin");
+
                     
                 }
                 else
